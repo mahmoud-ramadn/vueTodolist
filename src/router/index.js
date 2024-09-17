90199f4609fabcd2fe77b70e020ae37b2f845145
mahmoud-ramadn <mr9026573@gmail.com>
2024-09-17 23:44:21 +0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import TodosView from "@/views/TodosView.vue";
+
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'todos',
+            component:TodosView
+        }, {
+            path: "/about",
+            name: 'about',
+            component:()=>import('../views/AboutView.vue')
+        }
+    ]
+})
+
+export default router
